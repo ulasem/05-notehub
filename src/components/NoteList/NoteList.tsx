74b2381991d696351f244e305a4f1703b5234e31
Yuliia <semockoula@gmail.com>
2025-12-20 22:09:29 +0200

    update noteService.ts

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { Note } from '../../types/note';
-import { deleteNote } from '../../api/notes';
+import { deleteNote } from '../../services/noteService';
 import css from './NoteList.module.css';
 
 interface NoteListProps {
