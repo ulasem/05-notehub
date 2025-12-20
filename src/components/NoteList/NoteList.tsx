6e933655f261b44dd226e89b3394b289239bb69d
Yuliia Semochko <semockoula@gmail.com>
2025-12-20 21:27:14 +0200

    Replace onDelete prop with deleteMutation
    Refactor NoteList component to use useMutation for deleting notes.

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,21 +1,36 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { Note } from '../../types/note';
+import { deleteNote } from '../../api/notes';
 import css from './NoteList.module.css';
 
 interface NoteListProps {
   notes: Note[];
-  onDelete: (id: string) => void;
 }
 
-function NoteList({ notes, onDelete }: NoteListProps) {
+function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: (id: string) => deleteNote(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] });
+    },
+  });
+
   return (
     <ul className={css.list}>
       {notes.map(({ id, title, content, tag }) => (
         <li key={id} className={css.listItem}>
           <h2 className={css.title}>{title}</h2>
           <p className={css.content}>{content}</p>
+
           <div className={css.footer}>
             <span className={css.tag}>{tag}</span>
-            <button className={css.button} onClick={() => onDelete(id)}>
+            <button
+              className={css.button}
+              onClick={() => deleteMutation.mutate(id)}
+              disabled={deleteMutation.isPending}
+            >
               Delete
             </button>
           </div>
